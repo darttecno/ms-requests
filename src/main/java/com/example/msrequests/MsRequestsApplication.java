6c6357adc9b398394d29bc2c1c62cc8637ffd954
andressanchez2 <andres.sanchez@linktic.com>
2025-07-13 11:41:00 -0500

    Initial commit

---
+++ src/main/java/com/example/msrequests/MsRequestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.msrequests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRequestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsRequestsApplication.class, args);
+    }
+
+}
